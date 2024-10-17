0a96996890d349c444481b6d41bc1b255cd93c2a
NikolayBayzakov <nikolaybayzakov1@gmail.com>
2024-10-17 20:51:38 +0300

    Add files via upload

---
+++ practical work/7.1.py
@@ -0,0 +1,16 @@
+from random import randint
+
+lst = [randint(10, 80) for x in range(10)]
+print('Сгенерированный список', lst)
+
+unique_lst = set(lst)
+
+if len(lst) != unique_lst:
+    duplicate = set([i for i in unique_lst if lst.count(i) > 1])
+    if len(duplicate) >= 1:
+        print('Повторяющийся элемент: ', duplicate)
+    else:
+        print('В списке нет повторяющихся элементов')
+
+
+
---
+++ practical work/7.2.py
@@ -0,0 +1,10 @@
+from random import randint
+
+lst = [randint(1, 40) for x in range(15)]
+print('Первоначальный список:', lst)
+
+new_lst = [0 if x < 10 else 1 if x > 20 else x for x in lst]
+print('Измененный список:', new_lst)
+
+
+
